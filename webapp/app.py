54367e834d1ae57318181806800121e336705d8b
justkrismanohar <justkrismanohar@gmail.com>
2022-04-03 15:34:59 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 from flask import Flask
 
@@ -6,7 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello world! - Minizinc! From github!'
+    #return 'Hello world! - Minizinc! From github!'
+    p = subprocess.Popen("Your command", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    return 'Hello world! - Minizinc! From github!' + "<br/>" + str(p)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
