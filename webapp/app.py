f4b69761e855d3a1f90758868a26ebc905d4f207
justkrismanohar <justkrismanohar@gmail.com>
2022-04-03 15:38:24 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     #return 'Hello world! - Minizinc! From github!'
-    p = subprocess.Popen("Your command", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+    p = subprocess.Popen("minizinc --solvers", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
     return 'Hello world! - Minizinc! From github!' + "<br/>" + str(p)
 
 if __name__ == '__main__':
