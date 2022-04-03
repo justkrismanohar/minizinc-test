0608a389d57919585d081b850ec677a09db10f98
justkrismanohar <justkrismanohar@gmail.com>
2022-04-03 15:42:56 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def hello():
     #return 'Hello world! - Minizinc! From github!'
     p = subprocess.Popen("minizinc --solvers", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-    return 'Hello world! - Minizinc! From github!' + "<br/>" + str(p)
+    return 'Hello world! - Minizinc! From github!' + "<br/>" + p.decode("utf-8")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
